a05be43da81117f351fa8eb8ae0330bf531a1728
Thomas Watteyne <twatteyne@gmail.com>
2023-10-21 17:13:05 +0200

    #4. Fix.

--- 00std_leds_3_smiley/00std_leds_3_smiley.c
+++ 00std_leds_3_smiley/00std_leds_3_smiley.c
@@ -1,43 +1,126 @@
 #include "nrf52833.h"
 
-int main(void) {
+#define ROW1_DISCONNECT()    (NRF_P0->PIN_CNF[21]     = 0x00000002 )          // P0.21
+#define ROW2_DISCONNECT()    (NRF_P0->PIN_CNF[22]     = 0x00000002 )          // P0.22
+#define ROW3_DISCONNECT()    (NRF_P0->PIN_CNF[15]     = 0x00000002 )          // P0.15
+#define ROW4_DISCONNECT()    (NRF_P0->PIN_CNF[24]     = 0x00000002 )          // P0.24
+#define ROW5_DISCONNECT()    (NRF_P0->PIN_CNF[19]     = 0x00000002 )          // P0.19
+
+#define ROW1_OUTPUT()        (NRF_P0->PIN_CNF[21]     = 0x00000003 )          // P0.21
+#define ROW2_OUTPUT()        (NRF_P0->PIN_CNF[22]     = 0x00000003 )          // P0.22
+#define ROW3_OUTPUT()        (NRF_P0->PIN_CNF[15]     = 0x00000003 )          // P0.15
+#define ROW4_OUTPUT()        (NRF_P0->PIN_CNF[24]     = 0x00000003 )          // P0.24
+#define ROW5_OUTPUT()        (NRF_P0->PIN_CNF[19]     = 0x00000003 )          // P0.19
+
+#define ROW1_HIGH()          (NRF_P0->OUTSET          = (0x00000001 << 21) )   // P0.21
+#define ROW2_HIGH()          (NRF_P0->OUTSET          = (0x00000001 << 22) )   // P0.22
+#define ROW3_HIGH()          (NRF_P0->OUTSET          = (0x00000001 << 15) )   // P0.15
+#define ROW4_HIGH()          (NRF_P0->OUTSET          = (0x00000001 << 24) )   // P0.24
+#define ROW5_HIGH()          (NRF_P0->OUTSET          = (0x00000001 << 19) )   // P0.19
+                        
+#define COL1_DISCONNECT()    (NRF_P0->PIN_CNF[28]     = 0x00000002 )           // P0.28
+#define COL2_DISCONNECT()    (NRF_P0->PIN_CNF[11]     = 0x00000002 )           // P0.11
+#define COL3_DISCONNECT()    (NRF_P0->PIN_CNF[31]     = 0x00000002 )           // P0.31
+#define COL4_DISCONNECT()    (NRF_P1->PIN_CNF[05]     = 0x00000002 )           // P1.05
+#define COL5_DISCONNECT()    (NRF_P0->PIN_CNF[30]     = 0x00000002 )           // P0.30
+
+#define COL1_OUTPUT()        (NRF_P0->PIN_CNF[28]     = 0x00000003 )           // P0.28
+#define COL2_OUTPUT()        (NRF_P0->PIN_CNF[11]     = 0x00000003 )           // P0.11
+#define COL3_OUTPUT()        (NRF_P0->PIN_CNF[31]     = 0x00000003 )           // P0.31
+#define COL4_OUTPUT()        (NRF_P1->PIN_CNF[05]     = 0x00000003 )           // P1.05
+#define COL5_OUTPUT()        (NRF_P0->PIN_CNF[30]     = 0x00000003 )           // P0.30
+                             
+#define COL1_LOW()           (NRF_P0->OUTCLR          = (0x00000001 << 28) )   // P0.28
+#define COL2_LOW()           (NRF_P0->OUTCLR          = (0x00000001 << 11) )   // P0.11
+#define COL3_LOW()           (NRF_P0->OUTCLR          = (0x00000001 << 31) )   // P0.31
+#define COL4_LOW()           (NRF_P1->OUTCLR          = (0x00000001 << 05) )   // P1.05
+#define COL5_LOW()           (NRF_P0->OUTCLR          = (0x00000001 << 30) )   // P0.30
+
+typedef enum {
+    LED11,LED12,LED13,LED14,LED15,
+    LED21,LED22,LED23,LED24,LED25,
+    LED31,LED32,LED33,LED34,LED35,
+    LED41,LED42,LED43,LED44,LED45,
+    LED51,LED52,LED53,LED54,LED55
+} led_id_t;
+
+/**
+     12345
+   1 .....
+   2 .#.#.
+   3 .....
+   4 #...#
+   5 .###.
+*/
+
+const led_id_t LEDS_SMILEY[] = {
    
-    //=== set ROW1-ROW5 high
+          LED22,      LED24,
     
-    // ROW1 == P0.21
-    NRF_P0->PIN_CNF[21]           = 0x00000003;
-    NRF_P0->OUTSET                = (0x00000001 << 21);
-    // ROW2 == P0.22
-    NRF_P0->PIN_CNF[22]           = 0x00000003;
-    NRF_P0->OUTSET                = (0x00000001 << 22);
-    // ROW3 == P0.15
-    NRF_P0->PIN_CNF[15]           = 0x00000003;
-    NRF_P0->OUTSET                = (0x00000001 << 15);
-    // ROW4 == P0.24
-    NRF_P0->PIN_CNF[24]           = 0x00000003;
-    NRF_P0->OUTSET                = (0x00000001 << 24);
-    // ROW5 == P0.19
-    NRF_P0->PIN_CNF[19]           = 0x00000003;
-    NRF_P0->OUTSET                = (0x00000001 << 19);
+    LED41,                  LED45,
+          LED52,LED53,LED54
+};
 
-    //=== pull COL3 low
+void leds_all_off(void) {
+   
+    ROW1_DISCONNECT();
+    ROW2_DISCONNECT();
+    ROW3_DISCONNECT();
+    ROW4_DISCONNECT();
+    ROW5_DISCONNECT();
 
-    // COL1 == P0.28
-    NRF_P0->PIN_CNF[28]           = 0x00000003;
-    NRF_P0->OUTCLR                = (0x00000001 << 28);
-    // COL2 == P0.11
-    NRF_P0->PIN_CNF[11]           = 0x00000003;
-    NRF_P0->OUTCLR                = (0x00000001 << 11);
-    // COL3 == P0.31
-    NRF_P0->PIN_CNF[31]           = 0x00000003;
-    NRF_P0->OUTCLR                = (0x00000001 << 31);
-    // COL4 == P1.05
-    NRF_P1->PIN_CNF[5]            = 0x00000003;
-    NRF_P1->OUTCLR                = (0x00000001 << 5);
-    // COL5 == P0.30
-    NRF_P0->PIN_CNF[30]           = 0x00000003;
-    NRF_P0->OUTCLR                = (0x00000001 << 30);
+    COL1_DISCONNECT();
+    COL2_DISCONNECT();
+    COL3_DISCONNECT();
+    COL4_DISCONNECT();
+    COL5_DISCONNECT();
+}
 
-    // main loop
-    while(1);
+void leds_on(led_id_t led_id) {
+    
+    switch(led_id) {
+        
+        case LED11: ROW1_OUTPUT(); ROW1_HIGH(); COL1_OUTPUT(); COL1_LOW(); break;
+        case LED12: ROW1_OUTPUT(); ROW1_HIGH(); COL2_OUTPUT(); COL2_LOW(); break;
+        case LED13: ROW1_OUTPUT(); ROW1_HIGH(); COL3_OUTPUT(); COL3_LOW(); break;
+        case LED14: ROW1_OUTPUT(); ROW1_HIGH(); COL4_OUTPUT(); COL4_LOW(); break;
+        case LED15: ROW1_OUTPUT(); ROW1_HIGH(); COL5_OUTPUT(); COL5_LOW(); break;
+
+        case LED21: ROW2_OUTPUT(); ROW2_HIGH(); COL1_OUTPUT(); COL1_LOW(); break;
+        case LED22: ROW2_OUTPUT(); ROW2_HIGH(); COL2_OUTPUT(); COL2_LOW(); break;
+        case LED23: ROW2_OUTPUT(); ROW2_HIGH(); COL3_OUTPUT(); COL3_LOW(); break;
+        case LED24: ROW2_OUTPUT(); ROW2_HIGH(); COL4_OUTPUT(); COL4_LOW(); break;
+        case LED25: ROW2_OUTPUT(); ROW2_HIGH(); COL5_OUTPUT(); COL5_LOW(); break;
+
+        case LED31: ROW3_OUTPUT(); ROW3_HIGH(); COL1_OUTPUT(); COL1_LOW(); break;
+        case LED32: ROW3_OUTPUT(); ROW3_HIGH(); COL2_OUTPUT(); COL2_LOW(); break;
+        case LED33: ROW3_OUTPUT(); ROW3_HIGH(); COL3_OUTPUT(); COL3_LOW(); break;
+        case LED34: ROW3_OUTPUT(); ROW3_HIGH(); COL4_OUTPUT(); COL4_LOW(); break;
+        case LED35: ROW3_OUTPUT(); ROW3_HIGH(); COL5_OUTPUT(); COL5_LOW(); break;
+
+        case LED41: ROW4_OUTPUT(); ROW4_HIGH(); COL1_OUTPUT(); COL1_LOW(); break;
+        case LED42: ROW4_OUTPUT(); ROW4_HIGH(); COL2_OUTPUT(); COL2_LOW(); break;
+        case LED43: ROW4_OUTPUT(); ROW4_HIGH(); COL3_OUTPUT(); COL3_LOW(); break;
+        case LED44: ROW4_OUTPUT(); ROW4_HIGH(); COL4_OUTPUT(); COL4_LOW(); break;
+        case LED45: ROW4_OUTPUT(); ROW4_HIGH(); COL5_OUTPUT(); COL5_LOW(); break;
+
+        case LED51: ROW5_OUTPUT(); ROW5_HIGH(); COL1_OUTPUT(); COL1_LOW(); break;
+        case LED52: ROW5_OUTPUT(); ROW5_HIGH(); COL2_OUTPUT(); COL2_LOW(); break;
+        case LED53: ROW5_OUTPUT(); ROW5_HIGH(); COL3_OUTPUT(); COL3_LOW(); break;
+        case LED54: ROW5_OUTPUT(); ROW5_HIGH(); COL4_OUTPUT(); COL4_LOW(); break;
+        case LED55: ROW5_OUTPUT(); ROW5_HIGH(); COL5_OUTPUT(); COL5_LOW(); break;
+    }
+}
+
+int main(void) {
+    uint8_t i;
+
+    leds_on(LED11);
+
+    i=0;
+    while(1) {
+        leds_all_off();
+        leds_on(LEDS_SMILEY[i]);
+        i = (i+1)%sizeof(LEDS_SMILEY);
+    }
 }
