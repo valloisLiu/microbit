7f7afeacec978d1af30baef9bf009ad0c95ca972
Thomas Watteyne <twatteyne@gmail.com>
2023-10-21 15:56:39 +0200

    #2. working

--- 00std_led/00std_led.c
+++ 00std_led/00std_led.c
@@ -1,22 +1,35 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "nrf52833.h"
 
-/*********************************************************************
-*
-*       main()
-*
-*  Function description
-*   Application entry point.
-*/
 int main(void) {
-  int i;
+   
+    //=== set ROW3 high
+    
+    // ROW3 == P0.15
 
-  for (i = 0; i < 100; i++) {
-    printf("Hello World %d!\n", i);
-  }
-  do {
-    i++;
-  } while (1);
+    // configure as output
+    //  3           2            1           0
+    // 1098 7654 3210 9876 5432 1098 7654 3210
+    // .... .... .... .... .... .... .... ...A A: DIR:   1=Output
+    // .... .... .... .... .... .... .... ..B. B: INPUT: 1=Disconnect
+    // .... .... .... .... .... .... .... CC.. C: PULL:  0=Disabled
+    // .... .... .... .... .... .DDD .... .... D: DRIVE: 0=S0S1
+    // .... .... .... ..EE .... .... .... .... E: SENSE: 0=Disabled
+    // xxxx xxxx xxxx xx00 xxxx xxxx xxxx 0011 
+    //    0    0    0    0    0    0    0    3 0x00000003
+    NRF_P0->PIN_CNF[15]           = 0x00000003;
+
+    // set high
+    NRF_P0->OUTSET                = (0x00000001 << 15);
+
+    //=== pull COL3 low
+
+    // ROW3 == P0.31
+
+    NRF_P0->PIN_CNF[31]           = 0x00000003;
+
+    // set high
+    NRF_P0->OUTCLR                = (0x00000001 << 31);
+
+    // main loop
+    while(1);
 }
-
-/*************************** End of file ****************************/
