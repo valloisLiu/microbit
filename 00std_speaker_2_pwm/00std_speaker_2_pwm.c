7b063abeb6f7974217f60be3ab1cd24e65771d7c
Thomas Watteyne <twatteyne@gmail.com>
2023-10-22 07:08:46 +0200

    #10. dummy.

---
+++ 00std_speaker_2_pwm/00std_speaker_2_pwm.c
@@ -0,0 +1,19 @@
+#include "nrf52833.h"
+
+void wait(void) {
+    volatile uint32_t a;
+    for (a=0; a<0xffff;a++);
+}
+
+int main(void) {
+    
+    // set speaker pin as output
+    NRF_P0->PIN_CNF[0] = 0x00000003; // P0.00
+
+    while(1) {
+        NRF_P0->OUTSET = (0x00000001 << 0); // speaker pin high
+        wait();
+        NRF_P0->OUTCLR = (0x00000001 << 0); // speaker pin low
+        wait();
+    }
+}
