b42274b2ab4bf762214262fcce097a577d1d0df2
Thomas Watteyne <twatteyne@gmail.com>
2023-10-22 08:28:00 +0200

    #10. initial working code.

--- 00std_speaker_2_pwm/00std_speaker_2_pwm.c
+++ 00std_speaker_2_pwm/00std_speaker_2_pwm.c
@@ -1,19 +1,99 @@
 #include "nrf52833.h"
 
-void wait(void) {
-    volatile uint32_t a;
-    for (a=0; a<0xffff;a++);
-}
+// period = (16000000/freq_Hz)
+
+// we use a prescaler of 4, so the PWM counter runs at 16MHz/4=4MHz
+// period = (4000000/freq_Hz)
+
+#define NOTE_DO_3  15289 // 261.63 Hz
+#define NOTE_RE_3  13621 // 293.66 Hz
+#define NOTE_MI_3  12135 // 329.63 Hz
+#define NOTE_FA_3  11454 // 349.23 Hz
+#define NOTE_SOL_3 10204 // 392.00 Hz
+#define NOTE_LA_3   9091 // 440.00 Hz
+#define NOTE_SI_3   8099 // 493.88 Hz
+#define NOTE_DO_4   7645 // 523.25 Hz
+
+uint16_t pwm_comp[1] = {6000};
 
 int main(void) {
     
-    // set speaker pin as output
-    NRF_P0->PIN_CNF[0] = 0x00000003; // P0.00
+    // configure P0.00 as output
+    //  3           2            1           0
+    // 1098 7654 3210 9876 5432 1098 7654 3210
+    // .... .... .... .... .... .... .... ...A A: DIR:     1=Output
+    // .... .... .... .... .... .... .... ..B. B: INPUT:   1=Disconnect
+    // .... .... .... .... .... .... .... CC.. C: PULL:    0=Disabled
+    // .... .... .... .... .... .DDD .... .... D: DRIVE:   0=S0S1
+    // .... .... .... ..EE .... .... .... .... E: SENSE:   0=Disabled
+    // xxxx xxxx xxxx xx00 xxxx xxxx xxxx 0011 
+    //    0    0    0    0    0    0    0    3 0x00000003
+    NRF_P0->PIN_CNF[0]            = 0x00000003;
 
-    while(1) {
-        NRF_P0->OUTSET = (0x00000001 << 0); // speaker pin high
-        wait();
-        NRF_P0->OUTCLR = (0x00000001 << 0); // speaker pin low
-        wait();
-    }
+    //  3           2            1           0
+    // 1098 7654 3210 9876 5432 1098 7654 3210
+    // .... .... .... .... .... .... ...A AAAA A: PIN:      00 (P0.00)
+    // .... .... .... .... .... .... ..B. .... B: PORT:     0  (P0.00)
+    // C... .... .... .... .... .... .... .... C: CONNECT:  0=Connected
+    // 0xxx xxxx xxxx xxxx xxxx xxxx xx00 0000 
+    //    0    0    0    0    0    0    0    0 0x00000000
+    NRF_PWM0->PSEL.OUT[0]         = 0x00000000;
+
+    //  3           2            1           0
+    // 1098 7654 3210 9876 5432 1098 7654 3210
+    // .... .... .... .... .... .... .... ...A A: ENABLE:   1=Enabled
+    // 0xxx xxxx xxxx xxxx xxxx xxxx xxxx xxx1 
+    //    0    0    0    0    0    0    0    1 0x00000001
+    NRF_PWM0->ENABLE              = 0x00000001;
+
+    //  3           2            1           0
+    // 1098 7654 3210 9876 5432 1098 7654 3210
+    // .... .... .... .... .... .... .... ...A A: UPDOWN:   0=Up
+    // 0xxx xxxx xxxx xxxx xxxx xxxx xxxx xxx0 
+    //    0    0    0    0    0    0    0    0 0x00000000
+    NRF_PWM0->MODE                = 0x00000000;
+
+    //  3           2            1           0
+    // 1098 7654 3210 9876 5432 1098 7654 3210
+    // .... .... .... .... .... .... .... .AAA A: PRESCALER: 0=DIV_1
+    // 0xxx xxxx xxxx xxxx xxxx xxxx xxxx x000 
+    //    0    0    0    0    0    0    0    0 0x00000000
+    NRF_PWM0->PRESCALER           = 2;
+    
+    NRF_PWM0->COUNTERTOP          = 16000; // 16000@16MHz = 1kHz
+
+    //  3           2            1           0
+    // 1098 7654 3210 9876 5432 1098 7654 3210
+    // .... .... .... .... AAAA AAAA AAAA AAAA A: CNT:      0=Disabled
+    // 0xxx xxxx xxxx xxxx 0000 0000 0000 0000 
+    //    0    0    0    0    0    0    0    0 0x00000000
+    NRF_PWM0->LOOP                = 0x00000000;
+
+    //  3           2            1           0
+    // 1098 7654 3210 9876 5432 1098 7654 3210
+    // .... .... .... .... .... .... .... ..AA A: LOAD:     0=Common
+    // .... .... .... .... .... ...B .... .... B: MODE:     0=RefreshCount
+    // 0xxx xxxx xxxx xxxx xxxx xxx0 xxxx xx00 
+    //    0    0    0    0    0    0    0    0 0x00000000
+    NRF_PWM0->DECODER             = 0x00000000;
+    NRF_PWM0->SEQ[0].PTR          = (uint32_t)(pwm_comp);
+    NRF_PWM0->SEQ[0].CNT          = (sizeof(pwm_comp) / sizeof(uint16_t));
+
+    //  3           2            1           0
+    // 1098 7654 3210 9876 5432 1098 7654 3210
+    // .... .... AAAA AAAA AAAA AAAA AAAA AAAA A: CNT:      0=Continuous
+    // 0xxx xxxx 0000 0000 0000 0000 0000 0000 
+    //    0    0    0    0    0    0    0    0 0x00000000
+    NRF_PWM0->SEQ[0].REFRESH      = 0;
+
+    //  3           2            1           0
+    // 1098 7654 3210 9876 5432 1098 7654 3210
+    // .... .... AAAA AAAA AAAA AAAA AAAA AAAA A: CNT
+    // 0xxx xxxx 0000 0000 0000 0000 0000 0000 
+    //    0    0    0    0    0    0    0    0 0x00000000
+    NRF_PWM0->SEQ[0].ENDDELAY     = 0;
+
+    NRF_PWM0->TASKS_SEQSTART[0]   = 0x00000001;
+    
+    while(1);
 }
