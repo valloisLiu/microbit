ebdde5703fddeaa1e466162550b7b98bce0af7e6
Thomas Watteyne <twatteyne@gmail.com>
2023-10-22 18:23:58 +0200

    #19. removing UART RX

--- 00std_xgo_v2/00std_xgo_v2.c
+++ 00std_xgo_v2/00std_xgo_v2.c
@@ -32,11 +32,9 @@ uint8_t uartvars_rxBuf[9];
 int main(void) {
     
     // start hfclock
-    /*
     NRF_CLOCK->EVENTS_HFCLKSTARTED     = 0;
     NRF_CLOCK->TASKS_HFCLKSTART        = 0x00000001;
     while (NRF_CLOCK->EVENTS_HFCLKSTARTED == 0);
-    */
 
     // configure P0.04 (UART TX) as output
     //  3           2            1           0
@@ -55,29 +53,23 @@ int main(void) {
     // 1098 7654 3210 9876 5432 1098 7654 3210
     // .... .... .... .... .... .... .... ...A A: DIR:     0=Input
     // .... .... .... .... .... .... .... ..B. B: INPUT:   0=Connect
-    // .... .... .... .... .... .... .... CC.. C: PULL:    0=Disabled
+    // .... .... .... .... .... .... .... CC.. C: PULL:    3=Pullup
     // .... .... .... .... .... .DDD .... .... D: DRIVE:   0=S0S1
     // .... .... .... ..EE .... .... .... .... E: SENSE:   0=Disabled
-    // xxxx xxxx xxxx xx00 xxxx xxxx xxxx 0000 
-    //    0    0    0    0    0    0    0    0 0x00000000
-    NRF_P0->PIN_CNF[3]                 = 0x00000002;
+    // xxxx xxxx xxxx xx00 xxxx xxxx xxxx 1100 
+    //    0    0    0    0    0    0    0    c 0x0000000c
+    NRF_P0->PIN_CNF[3]                 = 0x0000000c;
 
     // configure
-    NRF_UARTE0->RXD.PTR                = (uint32_t)uartvars_rxBuf;
-    NRF_UARTE0->RXD.MAXCNT             = sizeof(uartvars_rxBuf);
     NRF_UARTE0->TXD.PTR                = (uint32_t)uartvars_txBuf;
     NRF_UARTE0->TXD.MAXCNT             = sizeof(uartvars_txBuf);
+    NRF_UARTE0->RXD.PTR                = (uint32_t)uartvars_rxBuf;
+    NRF_UARTE0->RXD.MAXCNT             = sizeof(uartvars_rxBuf);
     NRF_UARTE0->PSEL.TXD               = 0x00000004; // 0x00000004==P0.04
     NRF_UARTE0->PSEL.RXD               = 0x00000003; // 0x00000003==P0.03
     NRF_UARTE0->CONFIG                 = 0x00000000; // 0x00000000==no flow control, no parity bits, 1 stop bit
     NRF_UARTE0->BAUDRATE               = 0x01D7E000; // 0x01D7E000==Baud115200
-    //  3           2            1           0
-    // 1098 7654 3210 9876 5432 1098 7654 3210
-    // .... .... .... .... .... .... ..C. .... C: ENDRX_STARTRX
-    // .... .... .... .... .... .... .D.. .... D: ENDRX_STOPRX
-    // xxxx xxxx xxxx xxxx xxxx xxxx xx1x xxxx 
-    //    0    0    0    0    0    0    2    0 0x00000020
-    NRF_UARTE0->SHORTS                 = 0x00000020;
+    
     //  3           2            1           0
     // 1098 7654 3210 9876 5432 1098 7654 3210
     // .... .... .... .... .... .... .... ...A A: CTS
@@ -91,20 +83,15 @@ int main(void) {
     // .... .... .... I... .... .... .... .... I: RXSTARTED
     // .... .... ...J .... .... .... .... .... J: TXSTARTED
     // .... .... .L.. .... .... .... .... .... L: TXSTOPPED
-    // xxxx xxxx x0x0 0x0x xxxx xx01 0xx1 x000 
-    //    0    0    0    0    0    1    1    0 0x00000110
-    NRF_UARTE0->INTENSET               = 0x00000110;
+    // xxxx xxxx x0x0 0x0x xxxx xx01 0xx0 x000 
+    //    0    0    0    0    0    1    0    0 0x00000100
+    NRF_UARTE0->INTENSET               = 0x00000100;
     NRF_UARTE0->ENABLE                 = 0x00000008; // 0x00000008==enable
     
     // enable interrupts
     NVIC_SetPriority(UARTE0_UART0_IRQn, 1);
     NVIC_ClearPendingIRQ(UARTE0_UART0_IRQn);
     NVIC_EnableIRQ(UARTE0_UART0_IRQn);
-
-    // start receiving
-    NRF_UARTE0->EVENTS_RXSTARTED       = 0;
-    NRF_UARTE0->TASKS_STARTRX          = 0x00000001; // 0x00000001==start RX state machine; read received byte from RXD register
-    while (NRF_UARTE0->EVENTS_RXSTARTED == 0);
 
     memcpy(uartvars_txBuf,BUF_ACTION_SIT_DOWN,sizeof(BUF_ACTION_SIT_DOWN));
 
@@ -121,12 +108,5 @@ void UARTE0_UART0_IRQHandler(void) {
         // clear
         NRF_UARTE0->EVENTS_ENDTX = 0x00000000;
         
-    }
-
-    if (NRF_UARTE0->EVENTS_ENDRX == 0x00000001) {
-        // byte received from computer
-
-        // clear
-        NRF_UARTE0->EVENTS_ENDRX = 0x00000000;
     }
 }
